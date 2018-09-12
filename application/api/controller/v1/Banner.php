6aa9cbc61d5b9a2eed284e7b47afeaa743b7572a
deer <hiddendeerer@gmail.com>
2018-09-12 23:49:38 +0800

    上传thinkphp核心库

--- application/api/controller/v1/Banner.php
+++ application/api/controller/v1/Banner.php
@@ -1,17 +1,32 @@
 <?php
 namespace app\api\controller\v1;
 
+use think\Validate;
+
 class Banner
 {
 
     /**
-     * 获取指定id的banner信息 
+     * 获取指定id的banner信息
      * @url /banner/:id
      * @http GET
      * @id banner的id号
-     * 
+     *
      **/
-    public function getBanner ($id) {
+    public function getBanner($id)
+    {
+        $data = [
+            'name' => 'vendor',
+            'email' => 'vendor@qq.com'
+        ];
 
+        $validate = new Validate([
+            'name' => 'require|max:10',
+            'email' => 'email'
+        ]);
+
+        $result = $validate->check($data);
+        
+        dump($result);
     }
 }
