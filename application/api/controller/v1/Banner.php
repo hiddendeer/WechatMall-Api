302ffb786cc6ba229548ac4653e53944e142bbbd
hiddendeerer <hiddendeerer@gmail.com>
2018-09-17 15:21:17 +0800

    抛出JSON异常

--- application/api/controller/v1/Banner.php
+++ application/api/controller/v1/Banner.php
@@ -2,6 +2,8 @@
 namespace app\api\controller\v1;
 
 use app\api\validate\IDMustBePostiveInt;
+use app\api\model\Banner as BannerModel;
+use think\Exception;
 
 class Banner
 {
@@ -15,6 +17,19 @@ class Banner
      **/
     public function getBanner($id)
     {
+        //对传入的id检验
         (new IDMustBePostiveInt())->gocheck();
+        try {
+            $banner = BannerModel::getBannerById($id);
+        } 
+        catch (Exception $ex) {
+            $err = [
+                          'error_code' => 10001,
+                          'error_msg' => $ex->getMessage()
+                      ];
+            return json($err, 400);
+        }
+
+        return $banner;
     }
 }
---
+++ application/api/model/Banner.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\api\model;
+
+use think\Exception;
+
+class Banner
+{
+    public static function getBannerById($id)
+    {
+        //TODO：根据Banner ID号 获取banner信息
+        try {
+            1 / 0;
+            } 
+        catch (Exception $ex) {
+            throw $ex;
+        }
+        return "11111111";
+       
+    }
+}
