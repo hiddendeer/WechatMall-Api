d6ea6f1c217988bb64a4dc9aa521911a381583a4
hiddendeerer <hiddendeerer@gmail.com>
2018-10-15 15:20:25 +0800

    记录sql执行日记 7-7

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -9,8 +9,20 @@ class Banner
 {
     public static function getBannerById($id)
     {
-        $result = Db::query(
-            'select * from banner_item where banner_id = ?', [$id]);
+        //原生查询
+        // $result = Db::query(
+        //     'select * from banner_item where banner_id = ?', [$id]);
+        
+        //查询条件
+        $where_arr = [
+            'banner_id' => ['=',$id]
+        ];
+        //查询构造器
+        $result = Db::name('banner_item')
+        ->where(function ($query) use ($id) {
+            $query->where('banner_id','=',$id);
+        })
+        ->select();
 
         return $result;
     }
--- application/database.php
+++ application/database.php
@@ -16,7 +16,7 @@ return [
     // 服务器地址
     'hostname' => '127.0.0.1',
     // 数据库名
-    'database' => 'wechat',
+    'database' => 'zerg',
     // 用户名
     'username' => 'root',
     // 密码
